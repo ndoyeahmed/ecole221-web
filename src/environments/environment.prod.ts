640d7d8f3779a1b6a2375d1b628b4d0241b6bd30
Mouhamed <ndoyeahmed2602@gmail.com>
2023-09-06 15:55:09 +0000

    deploy on vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // url: 'http://cloud-28a1it3y.hosteur.net:8080/school'
+  url: 'https://momedrone-school-api-39624a6ff1f5.herokuapp.com'
   // url: 'http://localhost:8080/school'
   // url: 'http://localhost:8080'
-  url: 'https://gesaf221.com:8081/school'
+  // url: 'https://gesaf221.com:8081/school'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  // url: 'http://cloud-28a1it3y.hosteur.net:8080/school'
+  url: 'https://momedrone-school-api-39624a6ff1f5.herokuapp.com'
   // url: 'http://localhost:8080/school'
-  url: 'https://gesaf221.com:8081/school'
+  // url: 'https://gesaf221.com:8081/school'
   // url: 'http://localhost:8080'
 };
 
